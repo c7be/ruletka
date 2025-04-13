7925ea41b711c0918990ac5fad7639bb8f1686f9
c7be <167626392+c7be@users.noreply.github.com>
2025-04-13 13:18:45 +0200

    Update ruletka.py
    dodanie kilku komentarzy

--- ruletka.py
+++ ruletka.py
@@ -3,10 +3,10 @@ import random
 RED_NUMBERS = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}
 BLACK_NUMBERS = {2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35}
 
-def spin_wheel():
+def spin_wheel(): #spin the wheel!
     return random.randint(0, 36)
 
-def get_color(number):
+def get_color(number): #przypisanie kolorkow
     if number == 0:
         return "green"
     elif number in RED_NUMBERS:
@@ -62,6 +62,7 @@ def main():
 
         print(f"The wheel landed on: {result} ({result_color})")
 
+        #matma!
         if choice_type == "number" and (result == number_choice or cheat_mode):
             winnings = bet * 35
             print(f"You win! You earned ${winnings}")
